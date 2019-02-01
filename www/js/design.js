6be53a0390a79185704756d23de57ab020d99a23
HenryTacuchi <htacuchiv@outlook.com>
2019-02-01 18:24:37 -0500

    ui upgraded

--- www/js/design.js
+++ www/js/design.js
@@ -349,26 +349,6 @@ $('.menu button').each(function (index, value) {
 
 });
 
-$(window).load(function () {
-    $(".indexSection").center(true);
-});
-$(window).resize(function () {
-    $(".indexSection").center(true);
-});
-
-jQuery.fn.center = function (parent) {
-    if (parent) {
-        parent = this.parent();
-    } else {
-        parent = window;
-    }
-    this.css({
-        "position": "absolute",
-        "top": ((($(parent).height() - this.outerHeight()) / 2) + $(parent).scrollTop() - $('header').height() + "px"),
-        "left": ((($(parent).width() - this.outerWidth()) / 2) + $(parent).scrollLeft() + "px")
-    });
-    return this;
-}
 
 function highlightButtons() {
     var count = 0;
